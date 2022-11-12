bad62cefa6f85ab5c634ee16382a24aa09ecd699
Haim <hgq_2000@hotmail.com>
2022-11-12 14:34:58 -0600

    Creando nodo

--- Algorithms/Algorithms.cpp
+++ Algorithms/Algorithms.cpp
@@ -1,19 +1,36 @@
-// Algorithms.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+    // Listas (doblemente enlazadas, enlazada, circular simplemente enlazada, lista circular doblemente enlazada )
+    // Pilas
+    // Colas
+    // Arboles (de busqueda)
+    // Grafo
+    // Métodos de ordenamiento (Heap Sort, merge sort, bubble sort, insertion sort)
+void insertList(Nodo*& lista, int value);
+struct Nodo {
+    int value;
+    Nodo* next;
+};
 
 int main()
 {
-    std::cout << "Data Structures\n";
+    int x = 4;
+    int* p = &x;
+    Nodo* list = NULL;
+
+    std::cout << "Data Structures\n " << p << " " << &p;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+void insertList(Nodo*& lista, int newValue)
+{
+    //Create a new node and assign value
+    Nodo* newNode = new Nodo();
+    newNode->value = newValue;
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+    Nodo* aux1 = lista;
+    Nodo* aux2;
+
+    while (aux1 != NULL && aux1->value < newValue)
+    {
+
+    }
+}
